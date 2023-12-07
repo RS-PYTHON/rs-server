4aad728d197a4c12c8614d34459ba2b3fbfe6e9b
Ovidiu Pădeanu <opadeanu@c-s.ro>
2023-12-07 10:19:27 +0200

    [rspy14]: added tests and improved endpoint

--- src/CADIP/api/list_cadu.py
+++ src/CADIP/api/list_cadu.py
@@ -15,21 +15,23 @@ async def list_cadu_handler(station: str, start_date: str = None, stop_date: str
     if start_date and stop_date:
         # prepare start_stop
         dag_client = init_eodag(station)
-        products, number = dag_client.search(start = start_date, end = stop_date)
+        products, number = dag_client.search(start = start_date, end = stop_date, provider = station)
     return {station: prepare_products(products)}
 
 def get_station_ws(station: str):
     """Docstring will be here."""
-    stations_data = json.loads(open("CADIP/library/stations_cfg.json").read())
+    stations_data = json.loads(open("src/CADIP/library/stations_cfg.json").read())
     return stations_data.get(station.upper(), None)
 
 def init_eodag(station):
     """Docstring will be here."""
-    config_file_path = 'CADIP/library/cadip_ws_config.yaml'
+    config_file_path = 'src/CADIP/library/cadip_ws_config.yaml'
     eodag = EODataAccessGateway(config_file_path)
     eodag.set_preferred_provider(station)
     return eodag
 
 def prepare_products(products_list):
     """Docstring will be here."""
-    return [product.properties['Name'] for product in products_list]
+    if not products_list:
+        return []
+    return [(product.properties['id'], product.properties['Name']) for product in products_list]
---
+++ tests/test_cadip_list_endpoint.py
@@ -0,0 +1,32 @@
+"""Docstring to be added."""
+import pytest
+from fastapi.testclient import TestClient
+
+from src.CADIP.cadip_backend import app
+
+
+# TC-001 : User1 send a CURL request to a CADIP-Server on URL /cadip/{station}/cadu/list . 
+# He receives the list of CADU in the interval.
+@pytest.mark.unit
+@pytest.mark.parametrize("expected_chunk, expected_chunk_id, expected_chunk_name", 
+                         [
+                             (0, "2b17b57d-fff4-4645-b539-91f305c27c69", 
+                              "DCS_01_S1A_20170501121534062343_ch1_DSDB_00001.raw"),
+                             (1, "some_id_2", "S1A.raw"),
+                             (2, "some_id_3", "S2L1C.raw")
+                         ])
+def test_endpoint(expected_chunk, expected_chunk_id, expected_chunk_name):
+    """Docstring to be added."""
+    # Get all products between 2014 - 2023
+    start_date = "2014-01-01T12:00:00.000Z"
+    stop_date = "2023-12-30T12:00:00.000Z"
+    cadip_test_station = "CADIP"
+    rs_url = f"/cadip/{cadip_test_station}/cadu/list"
+    endpoint = f'{rs_url}?start_date="{start_date}"&stop_date="{stop_date}"'
+    client = TestClient(app)
+    # convert output to python dict
+    data = eval(client.get(endpoint).content.decode())
+    # check that request returned more than 1 element
+    assert len(data[cadip_test_station])
+    # Check if ids and names are matching with given parameters
+    assert data[cadip_test_station][expected_chunk] == [expected_chunk_id, expected_chunk_name]
