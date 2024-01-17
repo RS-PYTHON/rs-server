e95c09436cc71607bf144528f06e3f06548807d6
Ovidiu <opadeanu@c-s.ro>
2024-01-17 17:14:21 +0200

    Updated db model for services-tables

--- services/adgs/rs_server_adgs/adgs_download_status.py
+++ services/adgs/rs_server_adgs/adgs_download_status.py
@@ -1,16 +1,5 @@
-from sqlalchemy import Column, Enum
-
-from services.common.models.product_download_status import (
-    EDownloadStatus,
-    ProductDownloadStatus,
-)
+from services.common.db import Base
 
 
-class AdgsDownloadStatus(ProductDownloadStatus):
+class AdgsDownloadStatus(Base):
     __tablename__ = "adgs_download_status"
-    __table_args__ = {"extend_existing": True}
-    status: EDownloadStatus = Column(Enum(EDownloadStatus), default=EDownloadStatus.NOT_STARTED)
-
-    def __init__(self, *args, **kwargs):
-        """Invoked when creating a new record in the database table."""
-        super().__init__(*args, **kwargs)
--- services/cadip/rs_server_cadip/cadu_download_status.py
+++ services/cadip/rs_server_cadip/cadu_download_status.py
@@ -1,17 +1,5 @@
-from sqlalchemy import Column, DateTime, Enum, Integer, String, orm
-from services.common.models.product_download_status import (
-    EDownloadStatus,
-    ProductDownloadStatus,
-)
-
 from db import Base
 
 
 class CaduDownloadStatus(Base):
     __tablename__ = "cadu_download_status"
-    __table_args__ = {"extend_existing": True}
-    status: EDownloadStatus = Column(Enum(EDownloadStatus), default=EDownloadStatus.NOT_STARTED)
-
-    def __init__(self, *args, **kwargs):
-        """Invoked when creating a new record in the database table."""
-        super().__init__(*args, **kwargs)
