86faf56121b875c860f88d443ac89c11497cbff2
Julien Gaucher <julien.gaucher@csgroup.eu>
2024-01-18 11:19:06 +0100

    fix: configure eodag configuration

--- services/cadip/rs_server_cadip/cadip_retriever.py
+++ services/cadip/rs_server_cadip/cadip_retriever.py
@@ -1,6 +1,8 @@
 """This will be used to configure dataretriever used for cadip"""
 import json
+import os
 import os.path as osp
+import tempfile
 from pathlib import Path
 from typing import Any
 
@@ -9,9 +11,39 @@ from rs_server_common.data_retrieval.download_monitor import DownloadMonitor
 from rs_server_common.data_retrieval.eodag_provider import EodagProvider
 from rs_server_common.data_retrieval.provider import CreateProviderFailed
 from rs_server_common.data_retrieval.storage import Storage
+from rs_server_common.utils.logging import Logging
 
 CONF_FOLDER = Path(osp.realpath(osp.dirname(__file__))).parent / "config"
 EODAG_CONFIG = Path(osp.realpath(osp.dirname(__file__))).parent / "config" / "cadip_ws_config.yaml"
+
+logger = Logging.default(__name__)
+
+
+def get_eodag_provider(station) -> str:
+    """
+    Override the EODAG configuration file with values defined in environment variables.
+    Copy and modify the file under /tmp. Use it to init the EODAG provider.
+    """
+
+    # Read the generic configuration file
+    with open(EODAG_CONFIG, "r") as generic_file:
+        generic_data = generic_file.read()
+
+        # env variables to replace and default values
+        for var, default in {
+            "CADIP_STATION_USER": "test",
+            "CADIP_STATION_PASSWORD": "test",
+            "CADIP_STATION_HOST": "127.0.0.1",
+        }.items():
+            # e.g. replace ${CADIP_STATION_USER} by the CADIP_STATION_USER env var, or "test" by default
+            generic_data = generic_data.replace(f"${{{var}}}", os.environ.get(var, default))
+
+        # Write result in a temp file and return its path.
+        # The file is deleted on disk after exiting this block.
+        with tempfile.NamedTemporaryFile(mode="w+", suffix=".yaml", delete=True) as final_file:
+            final_file.write(generic_data)
+            final_file.flush()
+            return EodagProvider(Path(final_file.name), station)  # default to eodag, just init here
 
 
 def station_to_server_url(station: str) -> str | None:
@@ -76,7 +108,9 @@ def init_cadip_data_retriever(
     except ValueError as exc:
         raise CreateProviderFailed("Invalid station configuration") from exc
 
-    provider = EodagProvider(EODAG_CONFIG, station)  # default to eodag, just init here
+    # Override the EODAG configuration file with values defined in environment variables
+    provider = get_eodag_provider(station)
+
     # WIP, will be implemented with download
     cadip_storage: Storage = storage
     cadip_monitor: DownloadMonitor = download_monitor
