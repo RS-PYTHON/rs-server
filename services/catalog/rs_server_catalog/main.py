140ca67108ac2e2fabcee4e6818aee5c48fb192e
cyouri-csgroup <youri.colera@csgroup.eu>
2024-02-27 10:12:08 +0100

    fix: search endpoint without owner_id parameter 2

--- services/catalog/rs_server_catalog/main.py
+++ services/catalog/rs_server_catalog/main.py
@@ -68,16 +68,15 @@ def extract_openapi_specification():
     )
     openapi_spec_paths = openapi_spec["paths"]
     for key, _ in openapi_spec_paths.items():
-        new_key = "/catalog/{owner_id}" + key
+        new_key = f"/catalog{key}" if key == "/search" else "/catalog/{owner_id}" + key
         openapi_spec_paths[new_key] = openapi_spec_paths.pop(key)
         endpoint = openapi_spec_paths[new_key]
         for method_key, _ in endpoint.items():
             method = endpoint[method_key]
-            if method_key != "/catalog/search":
-                if "parameters" in method.keys():
-                    method["parameters"] = add_parameter_owner_id(method["parameters"])
-                else:
-                    method["parameters"] = add_parameter_owner_id([])
+            if "parameters" in method.keys():
+                method["parameters"] = add_parameter_owner_id(method["parameters"])
+            else:
+                method["parameters"] = add_parameter_owner_id([])
     app.openapi_schema = openapi_spec
     return app.openapi_schema
 
