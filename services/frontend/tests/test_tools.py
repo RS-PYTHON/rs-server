9087aad87f664427e67242119e711c9239a2375e
Fache Thomas (CSgroup) <thomas.fache@csgroup.eu>
2024-03-13 09:20:52 +0100

    feat: aggregated openapi procedure
    Refs: #RSPY-135

---
+++ services/frontend/tests/test_tools.py
@@ -0,0 +1,493 @@
+import json
+import re
+from dataclasses import dataclass
+from pathlib import Path
+
+import pytest
+import responses
+from requests import HTTPError
+from tools.openapi import BuildOpenapiFailed, build_aggregated_openapi
+
+
+@pytest.fixture(scope="module")
+def tools_test_path(resources_test_path) -> Path:
+    """The root path for test resources."""
+    return resources_test_path / "tools"
+
+
+@dataclass
+class ServicesConfiguration:
+    config: dict
+    file: Path
+
+    def doc_endpoint_url(self, service: str) -> str:
+        return (
+            f"{self.config[service]['root_url']}/{self.config[service]['doc_endpoint']}"
+        )
+
+
+@pytest.fixture(scope="module")
+def services_conf_file(tools_test_path) -> Path:
+    return tools_test_path / "services.json"
+
+
+@pytest.fixture(scope="module")
+def services_conf(services_conf_file) -> ServicesConfiguration:
+    with open(services_conf_file, "r") as file:
+        return ServicesConfiguration(config=json.load(file), file=services_conf_file)
+
+
+def a_openapi_path(service: str, response_type: str) -> dict:
+    return {
+        "get": {
+            "tags": [],
+            "summary": f"a summary {service}",
+            "description": f"a description {service}",
+            "operationId": f"operation_{service}",
+            "parameters": [
+                {
+                    "name": "name",
+                    "in": "query",
+                    "required": True,
+                    "schema": {
+                        "type": "string",
+                        "description": "name",
+                        "title": "Name",
+                    },
+                    "description": "the name",
+                },
+            ],
+            "responses": {
+                "200": {
+                    "description": "Successful Response",
+                    "content": {
+                        "application/json": {
+                            "schema": {"$ref": f"#/components/schemas/{response_type}"},
+                        },
+                    },
+                },
+                "422": {
+                    "description": "Validation Error",
+                    "content": {
+                        "application/json": {
+                            "schema": {
+                                "$ref": "#/components/schemas/HTTPValidationError",
+                            },
+                        },
+                    },
+                },
+            },
+        },
+    }
+
+
+def an_openapi_component(component_name: str) -> dict:
+    return {
+        "properties": {"is_ok": {"type": "boolean", "title": "Started"}},
+        "type": "object",
+        "required": ["is_ok"],
+        "title": component_name,
+        "description": f"{component_name} description.",
+    }
+
+
+def a_service_openapi(
+    service_name: str,
+    openapi_version: str = "3.1.0",
+    service_version: str = "0.1.0",
+    common_difference: str = "",
+) -> dict:
+    return {
+        "openapi": openapi_version,
+        "info": {"title": f"RS server {service_name}", "version": service_version},
+        "paths": {
+            "/common": a_openapi_path("common", "CommonResponse"),
+            f"/{service_name}": a_openapi_path(service_name, f"{service_name}Response"),
+        },
+        "components": {
+            "schemas": {
+                "CommonResponse": an_openapi_component(
+                    f"CommonResponse_{common_difference}",
+                ),
+                f"{service_name}Response": an_openapi_component(
+                    f"{service_name}Response",
+                ),
+            },
+        },
+    }
+
+
+@pytest.fixture(scope="module")
+def cadip_openapi(tools_test_path) -> dict:
+    return a_service_openapi(service_name="cadip", common_difference="cadip")
+
+
+@pytest.fixture(scope="module")
+def adgs_openapi(tools_test_path) -> dict:
+    return a_service_openapi(service_name="adgs", common_difference="adgs")
+
+
+class TestGenerateAggregateRestDoc:
+    @responses.activate
+    def test_calls_doc_endpoint_of_each_configured_service(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        cadip_resp = responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        adgs_resp = responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        build_aggregated_openapi(services_conf.file, tmp_path / "output.json")
+
+        assert cadip_resp.call_count == 1
+        assert adgs_resp.call_count == 1
+
+    @responses.activate
+    def test_writes_the_aggregated_openapi_in_the_given_file(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        output_file = tmp_path / "output.json"
+        build_aggregated_openapi(services_conf.file, output_file)
+
+        assert output_file.is_file()
+
+
+class TestGenerateAggregateRestDocFailsWhen:
+    @pytest.mark.parametrize(
+        ["conf_file", "message"],
+        [
+            pytest.param(
+                "not_found.json",
+                "File .*/not_found.json was not found.",
+                id="file not found",
+            ),
+            pytest.param(
+                "invalid.json",
+                "File .*/invalid.json content is invalid.",
+                id="file with bad format",
+            ),
+        ],
+    )
+    @responses.activate
+    def test_the_list_of_service_cannot_be_initialized(
+        self,
+        conf_file: str,
+        message: str,
+        tools_test_path: Path,
+        tmp_path: Path,
+    ):
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(
+                tools_test_path / conf_file,
+                tmp_path / "output.json",
+            )
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, IOError)
+        p = re.compile(message)
+        assert p.match(str(cause))
+
+    @responses.activate
+    def test_a_service_documentation_fails_to_be_retrieved(
+        self,
+        cadip_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=500,
+            json={"detail": "ADGS not happy."},
+        )
+
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, tmp_path / "output.json")
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, HTTPError)
+        assert str(cause) == "Unable to retrieve the openapi documentation for adgs."
+
+        assert isinstance(cause.__cause__, HTTPError)
+        assert cause.__cause__.response.status_code == 500
+        assert json.loads(cause.__cause__.response.content) == {
+            "detail": "ADGS not happy.",
+        }
+
+    @responses.activate
+    def test_the_documentation_of_a_service_is_invalid(
+        self,
+        cadip_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            body="{,}",
+        )
+
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, tmp_path / "output.json")
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, ValueError)
+        assert str(cause) == "The openapi documentation for adgs service is invalid."
+
+    @responses.activate
+    def test_the_merge_process_failed(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=a_service_openapi(service_name="adgs", openapi_version="9.9.9"),
+        )
+
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, tmp_path / "output.json")
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+    @responses.activate
+    def test_the_aggregated_documentation_cannot_be_written(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        output_file = tmp_path / "output.json"
+        output_file.mkdir()
+
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, output_file)
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, IOError)
+        assert (
+            str(cause) == f"Unable to write the aggregated openapi into {output_file}."
+        )
+
+
+class TestTheMergedOpenapi:
+    @pytest.fixture
+    @responses.activate
+    def the_merge_openapis(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ) -> dict:
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        output_file = tmp_path / "output.json"
+        build_aggregated_openapi(services_conf.file, output_file)
+
+        with open(output_file, "r") as file:
+            return json.load(file)
+
+    @responses.activate
+    def test_openapi_is_the_same_as_the_services(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert the_merge_openapis["openapi"] == cadip_openapi["openapi"]
+        assert the_merge_openapis["openapi"] == adgs_openapi["openapi"]
+
+    @responses.activate
+    def test_version_is_the_same_as_the_service(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert the_merge_openapis["info"]["version"] == cadip_openapi["info"]["version"]
+        assert the_merge_openapis["info"]["version"] == adgs_openapi["info"]["version"]
+
+    @responses.activate
+    def test_title_is_rs_server(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert the_merge_openapis["info"]["title"] == "RS server"
+
+    @responses.activate
+    def test_keeps_the_paths_of_all_services(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert all(
+            path in the_merge_openapis["paths"] for path in cadip_openapi["paths"]
+        )
+        assert all(
+            path in the_merge_openapis["paths"] for path in adgs_openapi["paths"]
+        )
+
+    def test_keeps_the_paths_configuration_of_one_of_the_service(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert the_merge_openapis["paths"]["/common"] in (
+            cadip_openapi["paths"]["/common"],
+            adgs_openapi["paths"]["/common"],
+        )
+
+    @responses.activate
+    def test_keeps_the_component_of_all_services(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert all(
+            component in the_merge_openapis["components"]["schemas"]
+            for component in cadip_openapi["components"]["schemas"]
+        )
+        assert all(
+            component in the_merge_openapis["components"]["schemas"]
+            for component in adgs_openapi["components"]["schemas"]
+        )
+
+    def test_keeps_the_component_configuration_of_one_of_the_service(
+        self,
+        cadip_openapi: dict,
+        adgs_openapi: dict,
+        the_merge_openapis: dict,
+    ):
+        assert the_merge_openapis["components"]["schemas"]["CommonResponse"] in (
+            cadip_openapi["components"]["schemas"]["CommonResponse"],
+            adgs_openapi["components"]["schemas"]["CommonResponse"],
+        )
+
+
+class TestTestMergeProcessFailedWhen:
+    @responses.activate
+    def test_all_services_have_not_the_same_openapi_version(
+        self,
+        cadip_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        adgs_openapi = a_service_openapi(service_name="adgs", openapi_version="9.9.9")
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        output_file = tmp_path / "output.json"
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, output_file)
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, ValueError)
+        assert str(cause) == "The openapi versions are not all the same : 9.9.9, 3.1.0"
+
+    @responses.activate
+    def test_all_services_have_not_the_same_version(
+        self,
+        cadip_openapi: dict,
+        services_conf: ServicesConfiguration,
+        tmp_path: Path,
+    ):
+        adgs_openapi = a_service_openapi(service_name="adgs", service_version="9.9.9")
+        responses.get(
+            url=services_conf.doc_endpoint_url("cadip"),
+            status=200,
+            json=cadip_openapi,
+        )
+        responses.get(
+            url=services_conf.doc_endpoint_url("adgs"),
+            status=200,
+            json=adgs_openapi,
+        )
+
+        with pytest.raises(BuildOpenapiFailed) as exc_info:
+            build_aggregated_openapi(services_conf.file, tmp_path / "output.json")
+        assert str(exc_info.value) == "Unable to generate REST documentation."
+
+        cause = exc_info.value.__cause__
+        assert isinstance(cause, ValueError)
+        assert str(cause) == "The service versions are not all the same : 9.9.9, 0.1.0"
---
+++ services/frontend/tools/openapi.py
@@ -0,0 +1,99 @@
+import json
+from functools import reduce
+from pathlib import Path
+
+import requests
+from requests import HTTPError
+
+
+class BuildOpenapiFailed(BaseException):
+    def __init__(self):
+        super().__init__("Unable to generate REST documentation.")
+
+
+def load_services_configuration(service_conf: Path) -> dict[str, dict]:
+    try:
+        with open(service_conf, "r") as file:
+            return json.load(file)
+    except IOError as e:
+        raise IOError(f"File {service_conf} was not found.") from e
+    except ValueError as e:
+        raise IOError(f"File {service_conf} content is invalid.") from e
+
+
+def load_openapi(service: str, retrieve_url: str) -> dict:
+    try:
+        response = requests.get(retrieve_url)
+        response.raise_for_status()
+        return json.loads(response.content)
+    except HTTPError as e:
+        # TODO check what kind of base exception is relevant here
+        raise HTTPError(
+            f"Unable to retrieve the openapi documentation for {service}.",
+        ) from e
+    except ValueError as e:
+        raise ValueError(
+            f"The openapi documentation for {service} service is invalid.",
+        ) from e
+
+
+def merge_dicts(current: dict, other: dict) -> dict:
+    current.update(other)
+    return current
+
+
+class AggregatedOpenapi:
+    def __init__(self, sub_openapis: dict[str, dict]):
+        self.sub_openapis = sub_openapis
+
+    def build_openapi(self) -> dict:
+        return {
+            "openapi": self.merge_openapi_versions(),
+            "info": {"title": "RS server", "version": self.merge_service_versions()},
+            "paths": self.merge_paths(),
+            "components": self.merge_components(),
+        }
+
+    def merge_openapi_versions(self) -> str:
+        openapi_versions = {sub["openapi"] for sub in self.sub_openapis.values()}
+        if len(openapi_versions) > 1:
+            versions = ", ".join(openapi_versions)
+            raise ValueError(f"The openapi versions are not all the same : {versions}")
+        return next(iter(openapi_versions))
+
+    def merge_service_versions(self) -> str:
+        services_versions = {
+            sub["info"]["version"] for sub in self.sub_openapis.values()
+        }
+        if len(services_versions) > 1:
+            versions = ", ".join(services_versions)
+            raise ValueError(f"The service versions are not all the same : {versions}")
+        return next(iter(services_versions))
+
+    def merge_paths(self) -> dict[str, dict]:
+        paths = (sub["paths"] for sub in self.sub_openapis.values())
+        return reduce(merge_dicts, paths)
+
+    def merge_components(self) -> dict[str, dict]:
+        schemas = (sub["components"]["schemas"] for sub in self.sub_openapis.values())
+        return {"schemas": reduce(merge_dicts, schemas)}
+
+
+def write_openapi(openapi: dict, to_path: Path):
+    try:
+        with open(to_path, "w") as file:
+            json.dump(openapi, file, indent=2)
+    except IOError as e:
+        raise IOError(f"Unable to write the aggregated openapi into {to_path}.") from e
+
+
+def build_aggregated_openapi(services_file: Path, to_path: Path):
+    try:
+        openapis: dict[str, dict] = {
+            service: load_openapi(service, f"{conf['root_url']}/{conf['doc_endpoint']}")
+            for service, conf in load_services_configuration(services_file).items()
+        }
+        aggregated = AggregatedOpenapi(openapis).build_openapi()
+        write_openapi(aggregated, to_path)
+    except BaseException as e:
+        raise BuildOpenapiFailed() from e
