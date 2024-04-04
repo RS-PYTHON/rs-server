b5a604bcc63979fed589499152cc238c4fb88af3
cyouri-csgroup <youri.colera@csgroup.eu>
2024-04-04 17:53:14 +0200

    fix: Too many public methods, adding landing_page.py

--- services/catalog/rs_server_catalog/user_catalog.py
+++ services/catalog/rs_server_catalog/user_catalog.py
@@ -25,6 +25,7 @@ from fastapi import HTTPException
 from pygeofilter.ast import Attribute, Equal, Like, Node
 from pygeofilter.parsers.cql2_json import parse as parse_cql2_json
 from pygeofilter.parsers.ecql import parse as parse_ecql
+from rs_server_catalog.landing_page import manage_landing_page
 from rs_server_catalog.user_handler import (
     add_user_prefix,
     filter_collections,
@@ -344,92 +345,6 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
         except KeyError as kerr_msg:
             raise HTTPException(detail=f"Missing key in request body! {kerr_msg}", status_code=400) from kerr_msg
 
-    def add_catalogs(self, request: Request, auth_roles: list, user_login: str, content: dict) -> dict:
-        """This function returns a list of links with all catalogs that the user has access to.
-        We use the list auth_roles to access to all authorizations for the user.
-        Args:
-            request (Request): The client request.
-            auth_roles (list): list of roles of the api-key.
-            user_login (str): The api-key owner.
-            content (dict): The landing page.
-
-        Returns:
-            dict: The updated content with all catalogs that the user has access to.
-        """
-        catalog_read_right_pattern = (
-            r"rs_catalog_(?P<owner_id>.*(?=:)):(?P<collection_id>.+)_(?P<right_type>read|write|download)(?=$)"
-        )
-        # Add catalogs links that the user can access to.
-        for role in auth_roles:
-            if match := re.match(catalog_read_right_pattern, role):
-                groups = match.groupdict()
-                if (
-                    user_login != groups["owner_id"]
-                    and groups["collection_id"] == "*"
-                    and groups["right_type"] == "read"
-                ):
-                    child_link = {
-                        "rel": "child",
-                        "type": "application/json",
-                        "href": "",
-                    }
-                    url = request.url
-                    child_link["href"] = f"{url}{groups['owner_id']}"
-                    content["links"].append(child_link)
-                    # content["links"] = [
-                    #     link for link in content["links"] if f"{groups['owner_id']}_" not in link["href"]
-                    # ]  # to remove all the collection links and keep only catalogs.
-        return content
-
-    def get_unauthorized_collections_links(self, auth_roles: list, content: dict) -> list:
-        """This function uses the authorisation roles list to get all unauthorized collections.
-
-        Args:
-            auth_roles (list): list of roles of the api-key.
-            content (dict): The landing page.
-
-        Returns:
-            list: The list of all unauthorized collections.
-        """
-        catalog_read_right_pattern = (
-            r"rs_catalog_(?P<owner_id>.*(?=:)):(?P<collection_id>.+)_(?P<right_type>read|write|download)(?=$)"
-        )
-        # Delete all collections that the user does not have access to.
-        unauthorized_collections = []
-        for link in content["links"]:  # Only check child links.
-            authorized = 0
-            if link["rel"] == "child":
-                for role in auth_roles:  # Check in the authorisations list if the link is allowed to the user.
-                    if match := re.match(catalog_read_right_pattern, role):
-                        groups = match.groupdict()
-                        if (
-                            groups["owner_id"] in link["href"]
-                            and (groups["collection_id"] == "*" or groups["collection_id"] in link["href"])
-                            and groups["right_type"] == "read"
-                        ):
-                            authorized = 1
-                            break
-                if not authorized:
-                    unauthorized_collections.append(link)
-        return unauthorized_collections
-
-    def manage_landing_page(self, request: Request, auth_roles: list, user_login: str, content: dict) -> dict:
-        """All sub user catalogs accessible by the user calling it are returned as "child" links.
-
-        Args:
-            request (Request): The client request.
-            auth_roles (list): list of roles of the api-key.
-            user_login (str): The api-key owner.
-            content (dict): The landing page.
-
-        Returns:
-            dict: The updated landingpage.
-        """
-        content = self.add_catalogs(request, auth_roles, user_login, content)
-        unauthorized_collections = self.get_unauthorized_collections_links(auth_roles, content)
-        content["links"] = [link for link in content["links"] if link not in unauthorized_collections]
-        return content
-
     def manage_all_collections(self, collections: dict, auth_roles: list, user_login: str) -> list:
         """Return the list of all collections accessible by the user calling it.
 
@@ -485,7 +400,7 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
                 try:
                     api_key = request.headers["x-api-key"]
                     auth_roles, _, user_login = await apikey_security(request, api_key)
-                    content = self.manage_landing_page(request, auth_roles, user_login, content)
+                    content = manage_landing_page(request, auth_roles, user_login, content)
                 except Exception as e:  # pylint: disable=broad-exception-caught
                     logging.exception(  # pylint: disable=logging-fstring-interpolation
                         f"apikey not available or local mode {e}",
