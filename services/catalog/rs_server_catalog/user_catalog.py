80e3c0eb05c0bb1b5e824572b6ddc6a0c75b0e60
cyouri-csgroup <youri.colera@csgroup.eu>
2024-04-04 17:33:45 +0200

    fix: simplification of the landing page function.

--- services/catalog/rs_server_catalog/user_catalog.py
+++ services/catalog/rs_server_catalog/user_catalog.py
@@ -344,9 +344,9 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
         except KeyError as kerr_msg:
             raise HTTPException(detail=f"Missing key in request body! {kerr_msg}", status_code=400) from kerr_msg
 
-    def manage_landing_page(self, request: Request, auth_roles: list, user_login: str, content: dict) -> dict:
-        """All sub user catalogs accessible by the user calling it are returned as "child" links.
-
+    def add_catalogs(self, request: Request, auth_roles: list, user_login: str, content: dict) -> dict:
+        """This function returns a list of links with all catalogs that the user has access to.
+        We use the list auth_roles to access to all authorizations for the user.
         Args:
             request (Request): The client request.
             auth_roles (list): list of roles of the api-key.
@@ -354,7 +354,7 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
             content (dict): The landing page.
 
         Returns:
-            dict: The updated landingpage.
+            dict: The updated content with all catalogs that the user has access to.
         """
         catalog_read_right_pattern = (
             r"rs_catalog_(?P<owner_id>.*(?=:)):(?P<collection_id>.+)_(?P<right_type>read|write|download)(?=$)"
@@ -379,13 +379,26 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
                     # content["links"] = [
                     #     link for link in content["links"] if f"{groups['owner_id']}_" not in link["href"]
                     # ]  # to remove all the collection links and keep only catalogs.
-        i = 0
-        # Delete all collections that the user does not have access to.
-        while i < len(content["links"]):
-            link = content["links"][i]
+        return content
 
-            if link["rel"] == "child":  # Only check child links.
-                authorized = 0
+    def get_unauthorized_collections_links(self, auth_roles: list, content: dict) -> list:
+        """This function uses the authorisation roles list to get all unauthorized collections.
+
+        Args:
+            auth_roles (list): list of roles of the api-key.
+            content (dict): The landing page.
+
+        Returns:
+            list: The list of all unauthorized collections.
+        """
+        catalog_read_right_pattern = (
+            r"rs_catalog_(?P<owner_id>.*(?=:)):(?P<collection_id>.+)_(?P<right_type>read|write|download)(?=$)"
+        )
+        # Delete all collections that the user does not have access to.
+        unauthorized_collections = []
+        for link in content["links"]:  # Only check child links.
+            authorized = 0
+            if link["rel"] == "child":
                 for role in auth_roles:  # Check in the authorisations list if the link is allowed to the user.
                     if match := re.match(catalog_read_right_pattern, role):
                         groups = match.groupdict()
@@ -397,11 +410,24 @@ class UserCatalogMiddleware(BaseHTTPMiddleware):
                             authorized = 1
                             break
                 if not authorized:
-                    del content["links"][i]
-                else:
-                    i += 1
-            else:
-                i += 1
+                    unauthorized_collections.append(link)
+        return unauthorized_collections
+
+    def manage_landing_page(self, request: Request, auth_roles: list, user_login: str, content: dict) -> dict:
+        """All sub user catalogs accessible by the user calling it are returned as "child" links.
+
+        Args:
+            request (Request): The client request.
+            auth_roles (list): list of roles of the api-key.
+            user_login (str): The api-key owner.
+            content (dict): The landing page.
+
+        Returns:
+            dict: The updated landingpage.
+        """
+        content = self.add_catalogs(request, auth_roles, user_login, content)
+        unauthorized_collections = self.get_unauthorized_collections_links(auth_roles, content)
+        content["links"] = [link for link in content["links"] if link not in unauthorized_collections]
         return content
 
     def manage_all_collections(self, collections: dict, auth_roles: list, user_login: str) -> list:
