80ec136f900e7e63b788f4021392c02011f8dc08
Ovidiu Pădeanu <opadeanu@c-s.ro>
2024-04-06 16:17:28 +0300

    Small docstr update

--- tests/conftest.py
+++ tests/conftest.py
@@ -221,9 +221,8 @@ def expected_products_fixture(a_product) -> list[dict]:
 
 
 def a_session_fixture(id_, at_date, satellite_idf):
-    """Fixture factory to build a dummy cadip session response.
-
-    :return: the factory function to build a cadip/aux product.
+    """
+        Function used to build full body of OData response from /Sessions.
     """
     return {
         "Id": "726f387b-ad2d-3538-8834-95e3cf8894c6",
