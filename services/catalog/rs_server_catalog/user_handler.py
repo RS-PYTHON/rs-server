b9223f43c5d157a3c9a83eafc5e07f86e9759299
Ovidiu <opadeanu@c-s.ro>
2024-06-11 11:58:15 +0300

    Fixed lint

--- services/catalog/rs_server_catalog/user_handler.py
+++ services/catalog/rs_server_catalog/user_handler.py
@@ -11,7 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-
+# pylint: disable=too-many-return-statements
 """This library contains all functions needed for the fastAPI middleware."""
 
 import re
