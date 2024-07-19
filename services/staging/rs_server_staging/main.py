f2954d4c1001b36b8229196fefec3dcb9f3255d1
Ovidiu <opadeanu@c-s.ro>
2024-07-19 15:02:37 +0300

    Added publish action + license headers

--- services/staging/rs_server_staging/main.py
+++ services/staging/rs_server_staging/main.py
@@ -1,28 +1,44 @@
+# Copyright 2024 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+import os
 import pathlib
+import uuid
+from typing import Dict
 
 from fastapi import FastAPI, HTTPException, Path
-from starlette.middleware.cors import CORSMiddleware
-from starlette.exceptions import HTTPException as StarletteHTTPException
-from starlette.requests import Request
-from starlette.responses import JSONResponse
 from pydantic import BaseModel
 from pygeoapi.api import API
 from pygeoapi.config import get_config
-import os
-from pydantic import BaseModel
-import uuid
-from .processors import processors
-from typing import Dict
-from tinydb import TinyDB, Query
+from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.middleware.cors import CORSMiddleware
+from starlette.requests import Request
+from starlette.responses import JSONResponse
+from tinydb import Query, TinyDB
 
-db = TinyDB('db.json')
-jobs_table = db.table('jobs')
+from .processors import processors
+
+db = TinyDB("db.json")
+jobs_table = db.table("jobs")
+
 
 # Use if you want to impose shaped-design of request
 class ExecuteRequest(BaseModel):
     job_id: str
     parameters: dict
     # Add any other fields you expect in the request body
+
 
 # Initialize a FastAPI application
 app = FastAPI(title="rs-staging", root_path="", debug=True)
@@ -44,17 +60,20 @@ app.add_middleware(
 # config = get_config(config_path)
 # openapi = openapi_path  # You should load the actual content of your OpenAPI spec here if it's not a file path
 
-api = API(get_config(os.environ['PYGEOAPI_CONFIG']), os.environ['PYGEOAPI_OPENAPI'])
+api = API(get_config(os.environ["PYGEOAPI_CONFIG"]), os.environ["PYGEOAPI_OPENAPI"])
+
 
 # Exception handlers
 @app.exception_handler(StarletteHTTPException)
 async def custom_http_exception_handler(request: Request, exc: StarletteHTTPException):
     return JSONResponse(status_code=exc.status_code, content={"message": exc.detail})
 
+
 # Health check route
 @app.get("/_mgmt/ping")
 async def ping():
     return {"status": "healthy"}
+
 
 # Endpoint to execute the staging process and generate a job ID
 @app.post("/processes/staging/execution")
@@ -67,12 +86,13 @@ async def execute_staging_process(request: dict):
         result = processor(parameters)
 
         # Store job status in TinyDB
-        jobs_table.insert({'job_id': job_id, 'status': 'completedOK'})
+        jobs_table.insert({"job_id": job_id, "status": "completedOK"})
 
         # Process result as needed and return a response
         return {"job_id": job_id, "message": "Process executed successfully", "result": result}
 
     raise HTTPException(status_code=404, detail=f"Processor '{processor_name}' not found")
+
 
 # Endpoint to get the status of a job by job_id
 @app.get("/jobs/{job_id}")
@@ -85,9 +105,6 @@ async def get_job_status(job_id: str = Path(..., title="The ID of the job")):
     else:
         raise HTTPException(status_code=404, detail="Job not found")
 
+
 # Mount pygeoapi endpoints
 app.mount(path="/oapi", app=api)
-
-if __name__ == "__main__":
-    import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
