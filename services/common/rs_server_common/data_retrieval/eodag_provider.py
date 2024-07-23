5b3fc3a646cddaddaa0e090413f6f2f05040c347
Ovidiu <opadeanu@c-s.ro>
2024-07-23 13:26:43 +0300

    Changed ADGS ptype

--- services/common/rs_server_common/data_retrieval/eodag_provider.py
+++ services/common/rs_server_common/data_retrieval/eodag_provider.py
@@ -143,7 +143,7 @@ class EodagProvider(Provider):
                 **mapped_search_args,  # type: ignore
                 provider=self.provider,
                 raise_errors=True,
-                productType="S1_SAR_RAW" if "adgs" not in self.provider.lower() else "GENERIC_PRODUCT_TYPE",
+                productType="S1_SAR_RAW" if "adgs" not in self.provider.lower() else "CAMS_GRF_AUX",
                 **kwargs,
             )
         except RequestError:
