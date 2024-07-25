17660a2c66899122534b068d513df58bbd4ba632
Ovidiu <opadeanu@c-s.ro>
2024-07-25 15:48:48 +0300

    Conftest update

--- services/staging/tests/conftest.py
+++ services/staging/tests/conftest.py
@@ -13,25 +13,24 @@
 # limitations under the License.
 
 """Module used to configure pytests."""
-
+# pylint: disable=wrong-import-position
 import os
 from pathlib import Path
+
+# set pygeoapi env variables
+geoapi_cfg = Path("tests/test_config.yml").absolute()
+os.environ["PYGEOAPI_CONFIG"] = str(geoapi_cfg)
+os.environ["PYGEOAPI_OPENAPI"] = ""
 
 import pytest
 from fastapi.testclient import TestClient
 
+from ..rs_server_staging.main import app
+
 
 @pytest.fixture(name="staging_client")
 def client_():
-    """set pygeoapi env variables and init fastapi client app."""
-    # To be updated
-    geoapi_cfg = Path("rs_server_staging/config/config.yml").absolute()
-    openapi_json = Path("rs_server_staging/config/openapi.json").absolute()
-    #
-    os.environ["PYGEOAPI_CONFIG"] = str(geoapi_cfg)
-    os.environ["PYGEOAPI_OPENAPI"] = str(openapi_json)
-    from ..rs_server_staging.main import app  # pylint: disable=import-outside-toplevel
-
+    """init fastapi client app."""
     # Test the FastAPI application, opens the database session
     with TestClient(app) as client:
         yield client
