3d6a493be91d69e433ca80585076d6520b15cd77
cyouri-csgroup <youri.colera@csgroup.eu>
2024-08-01 10:40:10 +0200

    fix: error logs

--- services/catalog/rs_server_catalog/user_catalog.py
+++ services/catalog/rs_server_catalog/user_catalog.py
@@ -336,7 +336,7 @@ class UserCatalog:  # pylint: disable=too-many-public-methods
             await self.client.get_collection(collection_id, request)
             return True
         except Exception as e:  # pylint: disable=broad-exception-caught
-            logger.error(" collection not found: %s", e)
+            logger.error("Collection %s not found: %s", collection_id, e)
             return False
 
     async def manage_search_request(  # pylint: disable=too-many-branches
