d30aa7071df7b4de2879bdbe0193530d612b2159
cyouri-csgroup <youri.colera@csgroup.eu>
2024-09-18 16:18:36 +0200

    feat: new delete assets and delete asset

--- services/catalog/rs_server_catalog/lifecycle.py
+++ services/catalog/rs_server_catalog/lifecycle.py
@@ -1,8 +1,14 @@
+import os
+
+import boto3
 import psycopg2
+from rs_server_common.s3_storage_handler.s3_storage_handler import S3StorageHandler
+
+BUCKET_NAME = "rs-cluster-catalog"
 
 
-# Function to detect expired items
 def check_expired_items(database_url):
+    """Select each item with a field 'expires' that is expired."""
 
     # Connect to the database
     try:
@@ -30,3 +36,32 @@ def check_expired_items(database_url):
         if connection:
             cursor.close()
             connection.close()
+
+
+def delete_assets_from_s3(expired_items):
+    """Delete all assets linked to expired items."""
+    for field in expired_items:
+        if "assets" in field:
+            for asset in field["assets"]:
+                delete_asset_from_s3(asset)
+            break
+
+
+def delete_asset_from_s3(asset):
+    """Delete one asset in a s3 bucket."""
+    try:
+        s3_handler = S3StorageHandler(
+            os.environ["S3_ACCESSKEY"],
+            os.environ["S3_SECRETKEY"],
+            os.environ["S3_ENDPOINT"],
+            os.environ["S3_REGION"],
+        )
+
+        prefix = f"s3://{BUCKET_NAME}/"
+        prefix_size = len(prefix)
+        s3_object = asset["href"][prefix_size:]
+        s3_handler.delete_file_from_s3(bucket=BUCKET_NAME, s3_obj=s3_object)
+    except KeyError as e:
+        raise RuntimeError("Could not find s3 credentials") from e
+    except Exception as e:
+        raise RuntimeError("General exception when trying to access bucket") from e
