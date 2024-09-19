d01bd828626098884d4f5ef43e9fb710ee2d4837
cyouri-csgroup <youri.colera@csgroup.eu>
2024-09-19 15:51:43 +0200

    feat: delete assets from bucket & tests

--- services/catalog/rs_server_catalog/lifecycle.py
+++ services/catalog/rs_server_catalog/lifecycle.py
@@ -1,3 +1,26 @@
+# Copyright 2024 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#  _     _  __                      _
+# | |   (_)/ _| ___  ___ _   _  ___| | ___
+# | |   | | |_ / _ \/ __| | | |/ __| |/ _ \
+# | |___| |  _|  __/ (__| |_| | (__| |  __/
+# |_____|_|_|  \___|\___|\__, |\___|_|\___|
+#                        |___/
+
+"""Control the data lifecycle with automatic cleanup of expired data"""
+
 import os
 
 import boto3
@@ -40,11 +63,13 @@ def check_expired_items(database_url):
 
 def delete_assets_from_s3(expired_items):
     """Delete all assets linked to expired items."""
-    for field in expired_items:
-        if "assets" in field:
-            for asset in field["assets"]:
-                delete_asset_from_s3(asset)
-            break
+    for expired_item in expired_items:
+        if len(expired_item) == 6:
+            content = expired_item[5]
+            if "assets" in content:
+                assets = content["assets"]
+                for asset in assets:
+                    delete_asset_from_s3(assets[asset])
 
 
 def delete_asset_from_s3(asset):
--- services/catalog/tests/test_life_cycle.py
+++ services/catalog/tests/test_life_cycle.py
@@ -1,116 +1,182 @@
+# Copyright 2024 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+#  _            _     _ _  __                      _
+# | |_ ___  ___| |_  | (_)/ _| ___  ___ _   _  ___| | ___
+# | __/ _ \/ __| __| | | | |_ / _ \/ __| | | |/ __| |/ _ \
+# | ||  __/\__ \ |_  | | |  _|  __/ (__| |_| | (__| |  __/
+#  \__\___||___/\__| |_|_|_|  \___|\___|\__, |\___|_|\___|
+#                                       |___/
+
 """Tests for life cycle catalog."""
 
 import json
+import os
 from datetime import datetime, timedelta
 from zoneinfo import ZoneInfo
 
 import psycopg2
-from rs_server_catalog.lifecycle import check_expired_items
+import requests
+from moto.server import ThreadedMotoServer
+from rs_server_catalog.lifecycle import (
+    check_expired_items,
+    delete_asset_from_s3,
+    delete_assets_from_s3,
+)
+from rs_server_common.s3_storage_handler.s3_storage_handler import S3StorageHandler
+
+from tests.test_endpoints import clear_aws_credentials, export_aws_credentials
 
 
-def test_check_expired_items(db_url):
-    """test the check expired items function."""
-    # Connect to the PostgreSQL database
-    connection = psycopg2.connect(db_url)
-    now = datetime.now(ZoneInfo("UTC"))
-    expired_date = (now - timedelta(days=30)).isoformat() + "Z"
+class TestCatalogLifeCycle:
+    """This class contains integration tests for the life cycle management."""
 
-    try:
-        # Create a cursor to execute the query
-        cursor = connection.cursor()
-
-        # Utilisez une date dans le passé pour datetime
-        past_date = datetime.now(ZoneInfo("UTC")) - timedelta(days=60)
-        expired_date = past_date + timedelta(days=30)
-
-        item = {
-            "id": "expired_item",
-            "stac_version": "1.0.0",
-            "collection": "toto_S1_L1",
-            "bbox": [-94.6334839, 37.0332547, -94.6005249, 37.0595608],
-            "geometry": {
-                "type": "Polygon",
-                "coordinates": [
-                    [
-                        [-94.6334839, 37.0595608],
-                        [-94.6334839, 37.0332547],
-                        [-94.6005249, 37.0332547],
-                        [-94.6005249, 37.0595608],
-                        [-94.6334839, 37.0595608],
-                    ],
+    # Utilisez une date dans le passé pour datetime
+    past_date = datetime.now(ZoneInfo("UTC")) - timedelta(days=60)
+    expired_date = past_date + timedelta(days=30)
+    item = {
+        "id": "expired_item",
+        "stac_version": "1.0.0",
+        "collection": "toto_S1_L1",
+        "bbox": [-94.6334839, 37.0332547, -94.6005249, 37.0595608],
+        "geometry": {
+            "type": "Polygon",
+            "coordinates": [
+                [
+                    [-94.6334839, 37.0595608],
+                    [-94.6334839, 37.0332547],
+                    [-94.6005249, 37.0332547],
+                    [-94.6005249, 37.0595608],
+                    [-94.6334839, 37.0595608],
                 ],
-            },
-            "properties": {
-                "datetime": past_date.isoformat(),
-                "gsd": 0.5971642834779395,
-                "width": 2500,
-                "height": 2500,
-                "proj:epsg": 3857,
-                "orientation": "nadir",
-                "owner_id": "toto",
-                "expires": expired_date.isoformat(),
-            },
-            "assets": {
-                "may24C355000e4102500n.tif": {
-                    "href": "s3://temp-bucket/toto_S1_L1/images/may24C355000e4102500n.tif",
-                    "type": "image/tiff; application=geotiff; profile=cloud-optimized",
-                    "title": "NOAA STORM COG",
-                },
-            },
-            "stac_extensions": [
-                "https://stac-extensions.github.io/eo/v1.0.0/schema.json",
-                "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
             ],
-        }
+        },
+        "properties": {
+            "datetime": past_date.isoformat(),
+            "gsd": 0.5971642834779395,
+            "width": 2500,
+            "height": 2500,
+            "proj:epsg": 3857,
+            "orientation": "nadir",
+            "owner_id": "toto",
+            "expires": expired_date.isoformat(),
+        },
+        "assets": {
+            "may24C355000e4102500n.tif": {
+                "href": "s3://catalog-bucket/toto_S1_L1/images/may24C355000e4102500n.tif",
+                "type": "image/tiff; application=geotiff; profile=cloud-optimized",
+                "title": "NOAA STORM COG",
+            },
+        },
+        "stac_extensions": [
+            "https://stac-extensions.github.io/eo/v1.0.0/schema.json",
+            "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
+        ],
+    }
 
-        cursor.execute("""ALTER TABLE _items_1 DROP CONSTRAINT _items_1_dt""")
+    def test_check_expired_items(self, db_url):
+        """test the check expired items function."""
+        # Connect to the PostgreSQL database
+        connection = psycopg2.connect(db_url)
+        now = datetime.now(ZoneInfo("UTC"))
 
-        cursor.execute(
-            """
-            INSERT INTO items (id, collection, geometry, datetime, end_datetime, content)
-            VALUES (%s, %s, %s, %s, %s, %s)
-        """,
-            (
-                item["id"],
-                item["collection"],
-                json.dumps(item["geometry"]),
-                past_date,
-                now,
-                json.dumps(
-                    {
-                        "bbox": item["bbox"],
-                        "assets": item["assets"],
-                        "properties": item["properties"],
-                        "stac_extensions": item["stac_extensions"],
-                    },
+        try:
+
+            # Create a cursor to execute the query
+            cursor = connection.cursor()
+            cursor.execute("""ALTER TABLE _items_1 DROP CONSTRAINT _items_1_dt""")
+
+            cursor.execute(
+                """
+                INSERT INTO items (id, collection, geometry, datetime, end_datetime, content)
+                VALUES (%s, %s, %s, %s, %s, %s)
+            """,
+                (
+                    self.item["id"],
+                    self.item["collection"],
+                    json.dumps(self.item["geometry"]),
+                    self.past_date,
+                    now,
+                    json.dumps(
+                        {
+                            "bbox": self.item["bbox"],
+                            "assets": self.item["assets"],
+                            "properties": self.item["properties"],
+                            "stac_extensions": self.item["stac_extensions"],
+                        },
+                    ),
                 ),
-            ),
-        )
+            )
 
-        # Commit the changes
-        connection.commit()
+            # Commit the changes
+            connection.commit()
 
-        print("Item inserted successfully.")
+            print("Item inserted successfully.")
 
-        # Verify insertion
-        verify_query = """
-        SELECT * FROM items WHERE id = %s;
-        """
-        cursor.execute(verify_query, (item["id"],))
-        inserted_item = cursor.fetchone()
+            # Verify insertion
+            verify_query = """
+            SELECT * FROM items WHERE id = %s;
+            """
+            cursor.execute(verify_query, (self.item["id"],))
+            inserted_item = cursor.fetchone()
 
-        if inserted_item:
-            print("Inserted item found:", inserted_item)
-        else:
-            print("No item found with the given ID.")
+            if inserted_item:
+                print("Inserted item found:", inserted_item)
+            else:
+                print("No item found with the given ID.")
 
-    except Exception as e:
-        print("Une erreur s'est produite :", e)
-        connection.rollback()
+        except Exception as e:
+            print("Une erreur s'est produite :", e)
+            connection.rollback()
 
-    finally:
-        # Close the cursor and connection
-        cursor.close()
-        connection.close()
-    expired_items = check_expired_items(database_url=db_url)
-    assert len(expired_items) == 1
+        finally:
+            # Close the cursor and connection
+            cursor.close()
+            connection.close()
+        expired_items = check_expired_items(database_url=db_url)
+        assert len(expired_items) == 1
+
+    def test_delete_assets_from_s3(self):
+        """Test used to verify that the function is correctly deleting the assing from the s3 bucket."""
+        # Create moto server and catalog-bucket
+        moto_endpoint = "http://localhost:8077"
+        catalog_bucket = "rs-cluster-catalog"
+        export_aws_credentials()
+        secrets = {"s3endpoint": moto_endpoint, "accesskey": None, "secretkey": None, "region": ""}
+        # Enable bucket transfer
+        os.environ["RSPY_LOCAL_CATALOG_MODE"] = "0"
+        server = ThreadedMotoServer(port=8077)
+        server.start()
+        try:
+            requests.post(f"{moto_endpoint}/moto-api/reset", timeout=5)
+            s3_handler = S3StorageHandler(
+                secrets["accesskey"],
+                secrets["secretkey"],
+                secrets["s3endpoint"],
+                secrets["region"],
+            )
+
+            s3_handler.s3_client.create_bucket(Bucket=catalog_bucket)
+
+            # Populate catalog-bucket with files.
+            lst_with_files_to_be_copied = ["s3://catalog-bucket/toto_S1_L1/images/may24C355000e4102500n.tif"]
+            for obj in lst_with_files_to_be_copied:
+                s3_handler.s3_client.put_object(Bucket=catalog_bucket, Key=obj, Body="testing\n")
+            delete_asset_from_s3(self.item["assets"]["may24C355000e4102500n.tif"])
+        except Exception as e:
+            raise RuntimeError("error") from e
+        finally:
+            server.stop()
+            clear_aws_credentials()
+            os.environ["RSPY_LOCAL_CATALOG_MODE"] = "1"
