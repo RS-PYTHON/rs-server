8e9d82cae61ad389f507d5cc2d858886668a04c6
Ovidiu <opadeanu@c-s.ro>
2025-01-20 13:46:21 +0200

    Sort after regroup

--- services/common/rs_server_common/stac_api_common.py
+++ services/common/rs_server_common/stac_api_common.py
@@ -603,7 +603,9 @@ class MockPgstac(ABC):  # pylint: disable=too-many-instance-attributes
 
             if file_results:
                 file_results = [Item(**feature) for feature in file_results[0]]
-                dict_data = ItemCollection(features=file_results, type="FeatureCollection").model_dump()
+                dict_data = sort_feature_collection(
+                    ItemCollection(features=file_results, type="FeatureCollection"), self.sortby,
+                ).model_dump()
         # Handle pagination links.
         if len(dict_data["features"]) > 0:
             # Don't create next page if the current one does not have features
