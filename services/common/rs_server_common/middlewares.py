cb942433ce8e1ca0882a1a6b335fe1cef9c85182
Ovidiu <opadeanu@c-s.ro>
2025-03-10 22:42:22 +0200

    fix fmt

--- services/common/rs_server_common/middlewares.py
+++ services/common/rs_server_common/middlewares.py
@@ -1,25 +1,40 @@
+# Copyright 2024 CS Group
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
 """Common functions for fastapi middlewares"""
 import traceback
 from typing import Callable
-from fastapi import Request
+
+from fastapi import Request, status
 from fastapi.responses import JSONResponse
-from fastapi import status
 from rs_server_common import settings as common_settings
 from rs_server_common.authentication import authentication, oauth2
 from rs_server_common.authentication.apikey import APIKEY_HEADER
 from rs_server_common.authentication.oauth2 import LoginAndRedirect
 from rs_server_common.utils.logging import Logging
-from starlette.middleware.base import BaseHTTPMiddleware
 from starlette.exceptions import HTTPException as StarletteHTTPException
+from starlette.middleware.base import BaseHTTPMiddleware
 
 logger = Logging.default(__name__)
+
 
 class AuthenticationMiddleware(BaseHTTPMiddleware):  # pylint: disable=too-few-public-methods
     """
     Implement authentication verification.
     """
 
-    def __init__(self, app, must_be_authenticated, dispatch = None):
+    def __init__(self, app, must_be_authenticated, dispatch=None):
         self.must_be_authenticated = must_be_authenticated
         super().__init__(app, dispatch)
 
