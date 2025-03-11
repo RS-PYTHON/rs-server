dc84f91ffc2e19667cef5ec31331468f96329cef
Ovidiu <opadeanu@c-s.ro>
2025-03-11 11:06:45 +0200

    Add back staging opentel

--- services/staging/rs_server_staging/main.py
+++ services/staging/rs_server_staging/main.py
@@ -39,6 +39,7 @@ from rs_server_common.middlewares import (
     apply_middlewares,
 )
 from rs_server_common.settings import CLUSTER_MODE, LOCAL_MODE
+from rs_server_common.utils import opentelemetry
 from rs_server_common.utils.logging import Logging
 from rs_server_common.utils.utils2 import filelock
 from rs_server_staging.processors import processors
@@ -360,6 +361,7 @@ if LOCAL_MODE:
 
 
 # Configure OpenTelemetry
+opentelemetry.init_traces(app, "rs.server.staging")
 
 app.include_router(router)
 app.router.lifespan_context = app_lifespan
