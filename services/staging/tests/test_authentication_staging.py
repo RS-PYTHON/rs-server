fc76082e12953649beb93bc4a232c92de900a09c
Ovidiu <opadeanu@c-s.ro>
2025-03-18 17:28:07 +0200

    Revert docstr

--- services/staging/tests/test_authentication_staging.py
+++ services/staging/tests/test_authentication_staging.py
@@ -12,7 +12,7 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
-"""Test staging endpoint authentication."""
+"""Unit tests for the authentication."""
 
 import pytest
 from pytest_httpx import HTTPXMock
