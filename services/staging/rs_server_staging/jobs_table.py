fdd04c408fb175ba9751d367f34339c7eb131665
mcolinde <matthieu.colin-de-verdiere@csgroup.eu>
2025-03-19 11:06:10 +0100

    fix: change some column names from the jobs table of the postgresSQL database

--- services/staging/rs_server_staging/jobs_table.py
+++ services/staging/rs_server_staging/jobs_table.py
@@ -48,8 +48,8 @@ class JobsTable(Base):  # pylint: disable=too-few-public-methods
     __tablename__ = "jobs"
 
     type = Column(Enum(JobType), nullable=False, server_default=JobType.process.value)
-    identifier = Column(String, primary_key=True, unique=True, index=True)
-    process_id = Column(String, nullable=False)
+    jobID = Column(String, primary_key=True, unique=True, index=True)
+    processID = Column(String, nullable=False)
     status = Column(Enum(JobStatus), nullable=False)
     progress = Column(Integer, server_default="0", nullable=False)
     # Pylint issue with func.now, check this: https://github.com/sqlalchemy/sqlalchemy/issues/9189
