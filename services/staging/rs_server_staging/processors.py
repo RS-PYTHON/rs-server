4da5e165c45885b8fb094de8582e336f0241a6f0
mcolinde <matthieu.colin-de-verdiere@csgroup.eu>
2025-04-10 11:02:47 +0200

    fix: pylint pass

--- services/staging/rs_server_staging/processors.py
+++ services/staging/rs_server_staging/processors.py
@@ -384,7 +384,7 @@ class Staging(
         self.station_token_list_lock = station_token_list_lock
 
     # Override from BaseProcessor, execute is async in RSPYProcessor
-    async def execute(
+    async def execute(  # pylint: disable=too-many-return-statements
         self,
         data: dict,
     ) -> tuple[str, dict]:
