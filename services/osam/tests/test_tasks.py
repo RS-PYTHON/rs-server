401213968e029b59fe5ad337f2de61a111f9a5e3
Alex Grosu <agrosu@c-s.ro>
2025-07-18 13:13:44 +0300

    Add one more use case for testing the update_s3_rights_lists function

--- services/osam/tests/test_tasks.py
+++ services/osam/tests/test_tasks.py
@@ -525,6 +525,34 @@ def test_build_s3_rights(user_info, expected):
                 ],
             },
         ),
+        (
+            {
+                "read": [
+                    "rspython-ops-catalog/no_collection",
+                    "rspython-ops-catalog-paul/*/*/",
+                ],
+                "read_download": [],
+                "write_download": [],
+            },
+            {
+                "Version": "2025-01-01",
+                "Statement": [
+                    {
+                        "Effect": "Allow",
+                        "Action": ["s3:ListBucket"],
+                        "Resource": "arn:aws:s3:::rspython-ops-catalog-paul",
+                        "Condition": {"StringLike": {"s3:prefix": ["*/*"]}},
+                    },
+                    {
+                        "Effect": "Allow",
+                        "Action": ["s3:GetBucketLocation"],
+                        "Resource": [
+                            "arn:aws:s3:::rspython-ops-catalog-paul/*",
+                        ],
+                    },
+                ],
+            },
+        ),
     ],
 )
 @patch("osam.tasks.datetime")
