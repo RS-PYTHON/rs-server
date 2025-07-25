83a015a5c0714d74d1f629d29f5553c468e02fcd
Vincent Privat <vincent.privat@airbus.com>
2025-07-25 11:08:51 +0200

    Update to STAC eo extension 2.0.0

--- services/catalog/tests/helpers.py
+++ services/catalog/tests/helpers.py
@@ -235,7 +235,7 @@ class Feature:
             },
             "stac_version": "1.0.0",
             "stac_extensions": [
-                "https://stac-extensions.github.io/eo/v1.0.0/schema.json",
+                "https://stac-extensions.github.io/eo/v2.0.0/schema.json",
                 "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
             ],
             "links": [{"href": "./.zattrs.json", "rel": "self", "type": "application/json"}],
--- services/catalog/tests/test_authentication_catalog.py
+++ services/catalog/tests/test_authentication_catalog.py
@@ -734,7 +734,7 @@ class TestAuthenticationGetOneItem:
             },
             "stac_version": "1.0.0",
             "stac_extensions": [
-                "https://stac-extensions.github.io/eo/v1.0.0/schema.json",
+                "https://stac-extensions.github.io/eo/v2.0.0/schema.json",
                 "https://stac-extensions.github.io/projection/v1.0.0/schema.json",
                 AUTHENT_EXTENSION,
             ],
