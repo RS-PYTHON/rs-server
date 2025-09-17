2035b02627f901a16e3783ac5e97407e21471042
nleconte-csgroup <86782407+nleconte-csgroup@users.noreply.github.com>
2025-09-17 12:50:35 +0200

    fix: apt list debian instead of ubuntu

--- .github/scripts/git_debug_image.sh
+++ .github/scripts/git_debug_image.sh
@@ -26,7 +26,8 @@ ROOT_DIR="$(realpath $SCRIPT_DIR/..)"
 BRANCH_NAME="$1" # git branch name
 
 # Install components in the docker images
-echo "deb http://archive.ubuntu.com/ubuntu $(lsb_release -cs) main universe" > /etc/apt/sources.list
+. /etc/os-release
+echo "deb http://deb.debian.org/debian $VERSION_CODENAME main" > /etc/apt/sources.list
 apt update
 apt install -y git vim emacs-nox
 
