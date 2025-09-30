e192931a508a0113239963f0f9aa4cedb91c3062
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-09-30 16:56:39 +0200

    Use layer-cleanup and restore-apt

--- .github/scripts/git_debug_image.sh
+++ .github/scripts/git_debug_image.sh
@@ -26,8 +26,7 @@ ROOT_DIR="$(realpath $SCRIPT_DIR/..)"
 BRANCH_NAME="$1" # git branch name
 
 # Install components in the docker images
-. /etc/os-release
-echo "deb http://deb.debian.org/debian $VERSION_CODENAME main" > /etc/apt/sources.list
+restore-apt.sh
 apt update
 apt install -y --no-install-recommends git vim-tiny mg
 
@@ -60,4 +59,4 @@ done
 chown -R user:user /home/user/operational-services
 
 # Clean everything
-rm -rf /tmp/whl /root/.cache/pip /var/cache/apt/archives /var/lib/apt/lists/* /etc/apt/sources.list
+layer-cleanup.sh
