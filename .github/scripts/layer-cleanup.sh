a80ae63b4e480745ec133fcf43dbf2fed3f72bcb
Julien Gaucher <julien.gaucher@csgroup.eu>
2025-10-01 11:54:16 +0200

    Use layer-cleanup and restore-apt

--- .github/scripts/layer-cleanup.sh
+++ .github/scripts/layer-cleanup.sh
@@ -17,7 +17,6 @@ apt-get autoclean --yes
 apt-get autoremove --yes
 
 rm -rf /var/lib/apt/lists/*
-rm -rf /etc/apt/sources.list.d/*
 rm -rf /usr/local/src/*
 
 rm -rf /var/cache/apt/*
@@ -30,5 +29,10 @@ rm -rf /tmp/* /var/tmp/*
 rm -rf /opt/conda/pkgs/cache
 
 rm -rf /tmp/whl
+
+# WARNING: this removes the apt repository list. To restore it and be able to run 'apt update',
+# you must run (works only in Debian):
+# . /etc/os-release && echo "deb http://deb.debian.org/debian $VERSION_CODENAME main" > /etc/apt/sources.list
+rm -rf /etc/apt/sources.list.d/*
 
 exit 0
