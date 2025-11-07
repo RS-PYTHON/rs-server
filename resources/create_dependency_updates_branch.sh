78d04f36ce1c43006647f8bd904617002b9fc57d
Vincent Privat <vincent.privat@airbus.com>
2025-11-07 10:43:00 +0100

    Dependency updates

--- resources/create_dependency_updates_branch.sh
+++ resources/create_dependency_updates_branch.sh
@@ -20,9 +20,27 @@ git checkout develop || exit 20
 git pull || exit 30
 git clean -f || exit 40
 git checkout -b "updates-$d" || exit 60
-for s in common adgs cadip prip catalog staging frontend ; do cd $s && poetry lock --regenerate && poetry show -o && cd - ; done
+
+fix_psycopg2_lock() {
+  # restore line wrongly deleted by poetry
+  if grep -q "6ecddcf573777536bddfefaea8079ce959287798c8f5804bee6933635d538" poetry.lock; then
+    if ! grep -q "964d31caf728e217c697ff77ea69c2ba0865fa41ec20bb00f0977e62fdcc52e3" poetry.lock; then
+      sed -i '/6ecddcf573777536bddfefaea8079ce959287798c8f5804bee6933635d538/a\    {file = "psycopg2-2.9.11.tar.gz", hash = "sha256:964d31caf728e217c697ff77ea69c2ba0865fa41ec20bb00f0977e62fdcc52e3"},' poetry.lock
+    fi
+  fi
+}
+
+for s in common adgs cadip prip catalog staging frontend ; do
+  cd "$s"
+  poetry lock --regenerate
+  fix_psycopg2_lock
+  poetry show -o
+  cd - >/dev/null
+done
+
 cd .. || exit 70
 poetry lock --regenerate || exit 80
+fix_psycopg2_lock
 poetry show -o || exit 85
 git add . || exit 90
 git commit -m "Dependency updates" || exit 100
