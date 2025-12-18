96dc753ac7107394b289d38bfd2622d2eed575e7
Ovidiu <ovidiu.padeanu@cs-soprasteria.com>
2025-12-18 17:01:15 +0200

    Updated with new header

--- services/common/rs_server_common/ftp_handler/ftp_handler.py
+++ services/common/rs_server_common/ftp_handler/ftp_handler.py
@@ -1,4 +1,4 @@
-# Copyright 2025 CS Group
+# Copyright 2023-2025 Airbus, CS Group
 #
 # Licensed under the Apache License, Version 2.0 (the "License");
 # you may not use this file except in compliance with the License.
@@ -11,6 +11,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
+
 """
 FTP/FTPS client module.
 Provides a unified interface for connecting to FTP or FTPS servers,
