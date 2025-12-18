cbc5ea26820fa4587da34e9b6469587d27ce818d
Ovidiu <ovidiu.padeanu@cs-soprasteria.com>
2025-12-18 15:02:37 +0200

    fix pytest

--- services/common/tests/ftp_handler/test_ftp_client.py
+++ services/common/tests/ftp_handler/test_ftp_client.py
@@ -56,9 +56,9 @@ def test_init_station_mode(monkeypatch):
     monkeypatch.setenv("TEST_PASS", "pass1")
     monkeypatch.setenv("TEST_PORT", "2121")
     monkeypatch.setenv("USE_SSL", "true")
-    monkeypatch.setenv("CA_CERT", "ca.pem")
-    monkeypatch.setenv("CLIENT_CERT", "client.pem")
-    monkeypatch.setenv("CLIENT_KEY", "client.key")
+    monkeypatch.setenv("TEST_CA_CERT", "ca.pem")
+    monkeypatch.setenv("TEST_CLIENT_CERT", "client.pem")
+    monkeypatch.setenv("TEST_CLIENT_KEY", "client.key")
 
     client = FTPClient(station="test")
 
