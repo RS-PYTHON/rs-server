fd9ef06dc8b024303431889f5ae769372b743e61
Alin BUTU <alin.butu@c-s.ro>
2026-01-21 22:46:03 +0200

    externalIds suport for adgs

--- services/common/rs_server_common/stac_api_common.py
+++ services/common/rs_server_common/stac_api_common.py
@@ -603,6 +603,15 @@ class MockPgstac(ABC):  # pylint: disable=too-many-instance-attributes
                 )
             read_property(prop, value)
 
+        # Read externalIds from GET/POST parameters when supported by queryables
+        external_ids_param = params.pop("externalIds", None)
+        if external_ids_param is None:
+            external_ids_param = self.request.query_params.get("externalIds")
+        if external_ids_param is None and post_json_body:
+            external_ids_param = post_json_body.get("externalIds")
+        if external_ids_param is not None and "externalIds" in allowed_properties:
+            read_property("externalIds", external_ids_param)
+
         # map stac platform/constellation values to odata values...
         mission = self.map_mission(stac_params.get("platform"), stac_params.get("constellation"))
         # ... still saved with stac keys for now
