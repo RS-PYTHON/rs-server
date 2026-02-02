a6f597f254cedc7b3464f315a0fd0c185a3c3063
astanga-cs <eastinga@AAP-CZC550735P.localdomain>
2026-02-02 16:28:46 +0200

    changed the port to internal & commented chker for now

--- services/frontend/resources/build_aggregated_openapi.sh
+++ services/frontend/resources/build_aggregated_openapi.sh
@@ -34,7 +34,9 @@ if [[ " $@ " == *" --run-services "* ]]; then
     # On exit, kill the containers and network and send the exit signal to subprocesses
     pg_container="postgres"
     pgstac_container="pgstac"
-    on_exit="docker rm -f $pg_container $pgstac_container || true; docker network rm $network || true"
+    dpr_container="rs-dpr-service"
+
+    on_exit="docker rm -f $pg_container $pgstac_container $dpr_container || true; docker network rm $network || true"
     trap 'eval $on_exit' EXIT # use simple quotes so the string is interpreted when we exit
 
     # Use the same configuration as in the cluster deployment.
@@ -82,19 +84,18 @@ if [[ " $@ " == *" --run-services "* ]]; then
 
     # DPR service
     dpr_image="ghcr.io/rs-python/rs-dpr-service:latest"
-    dpr_container="rs-dpr-service"
 
     if ! docker ps --format '{{.Names}}' | grep -q "^$dpr_container\$"; then
         docker pull "$dpr_image"
         docker run --rm --network=$network --name=$dpr_container \
             -e RSPY_LOCAL_MODE=1 \
-            -e POSTGRES_HOST \
+            -e POSTGRES_HOST=postgres \
             -e POSTGRES_PORT \
             -e POSTGRES_DB \
             -e POSTGRES_USER \
             -e POSTGRES_PASSWORD \
             -p 6003:8000 \
-            --health-cmd="curl -f http://127.0.0.1:8000/_mgmt/ping || exit 1" \
+            --health-cmd="python3 -c 'import requests; assert requests.get(\"http://127.0.0.1:8000/_mgmt/ping\").ok'" \
             --health-interval=2s \
             --health-timeout=2s \
             --health-retries=10 \
@@ -104,13 +105,13 @@ if [[ " $@ " == *" --run-services "* ]]; then
         echo "Using already running DPR container: $dpr_container"
     fi
 
-    i=0
-    while [[ $(docker inspect --format='{{.State.Health.Status}}' $dpr_container) != healthy ]]; do
-        sleep 2
-        i=$((i+1))
-        ((i>=20)) && >&2 echo "Error: DPR container '$dpr_container' is not healthy after $((i*2))s" && exit 1
-    done
-    echo "DPR service container is healthy"
+    # i=0
+    # while [[ $(docker inspect --format='{{.State.Health.Status}}' $dpr_container) != healthy ]]; do
+    #     sleep 2
+    #     i=$((i+1))
+    #     ((i>=20)) && >&2 echo "Error: DPR container '$dpr_container' is not healthy after $((i*2))s" && exit 1
+    # done
+    # echo "DPR service container is healthy"
 
     # Run local fastapi services
     run_local_service() {
