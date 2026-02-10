631eff22c8f22b03a6446a2ecf0b6ac90434dbf0
Ovidiu <ovidiu.padeanu@cs-soprasteria.com>
2026-02-10 14:14:54 +0200

    Osam frontend (2)

--- services/frontend/resources/build_aggregated_openapi.sh
+++ services/frontend/resources/build_aggregated_openapi.sh
@@ -155,6 +155,7 @@ if [[ " $@ " == *" --run-services "* ]]; then
         run_local_service "../staging" "rs_server_staging.main:app" 8004 "_mgmt/ping"
     run_local_service "../prip" "rs_server_prip.fastapi.prip_app:app" 8005 "health"
     run_local_service "../edrs" "rs_server_edrs.fastapi.edrs_app:app" 8006 "health"
+    run_local_service "../osam" "rs_server_osam.main:app" 7001 "health"
 
     # Use pgstac database
     PGDATABASE=$POSTGRES_DB \
