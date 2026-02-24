f03f66acc478695f45143b1ade212c0d2ecbb080
astanga-cs <eastinga@AAP-CZC550735P.localdomain>
2026-02-24 16:02:06 +0200

    Summaries for cadip/auxip/prip

--- services/common/rs_server_common/stac_api_common.py
+++ services/common/rs_server_common/stac_api_common.py
@@ -60,6 +60,8 @@ from rs_server_common.utils.cql2_filter_extension import process_filter_extensio
 from rs_server_common.utils.logging import Logging
 from rs_server_common.utils.utils import (
     extract_eo_product,
+    find_product_type,
+    map_stac_platform,
     odata_to_stac,
     run_in_threads,
     validate_inputs_format,
@@ -267,6 +269,45 @@ class MockPgstac(ABC):  # pylint: disable=too-many-instance-attributes
 
         return queryables
 
+    def build_summaries(self, query: dict) -> dict | None:
+        """
+        Builds summaries for CADIP/AUXIP/PRIP collections.
+
+        Returns a dict:
+            - {"platform": [...]} for CADIP
+            - {"product:type": [...]} for AUXIP/PRIP
+        """
+        if self.service == "cadip":
+            satellites = query.get("Satellite") or []
+            if isinstance(satellites, str):
+                satellites = [s.strip() for s in satellites.split(",")]
+
+            platforms = set()
+
+            for sat in satellites:
+                for satellite in map_stac_platform()["satellites"]:
+                    for key, info in satellite.items():
+                        if info.get("code") == sat:
+                            platforms.add(key)
+
+            results = sorted(platforms)
+            return {"platform": results}
+
+        products = query.get("productType") or []
+        if isinstance(products, str):
+            products = [s.strip() for s in products.split(",")]
+
+        ptype = set()
+        if self.service == "prip":
+            for p in products:
+                ptype.add(find_product_type(p)["productType"])
+        else:
+            for p in products:
+                ptype.add(p)
+
+        results = sorted(ptype)
+        return {"product:type": results}
+
     async def fetchval(self, query, *args, column=0, timeout=None):  # pylint: disable=unused-argument
         """Run a query and return a value in the first row.
 
@@ -297,6 +338,10 @@ class MockPgstac(ABC):  # pylint: disable=too-many-instance-attributes
                     status.HTTP_404_NOT_FOUND,
                     f"Unknown {self.service} collection: {collection_id!r}",
                 )
+
+            query = collection.get("query")
+            if query:
+                collection["summaries"] = self.build_summaries(query)
 
             # Convert into stac object (to ensure validity) then back to dict
             collection.setdefault("stac_version", DEFAULT_STAC_VERSION)
