29e35632bea066bb72623dbaa832d9458088d452
Alin BUTU <alin.butu@c-s.ro>
2026-02-25 10:57:05 +0200

    fix staging lock in swagger ui

--- services/frontend/resources/build_aggregated_openapi.sh
+++ services/frontend/resources/build_aggregated_openapi.sh
@@ -151,8 +151,7 @@ if [[ " $@ " == *" --run-services "* ]]; then
     # Use postgres database
     run_local_service "../adgs" "rs_server_adgs.fastapi.adgs_app:app" 8001 "health"
     run_local_service "../cadip" "rs_server_cadip.fastapi.cadip_app:app" 8002 "health"
-    RSPY_LOCAL_MODE=1 \
-        run_local_service "../staging" "rs_server_staging.main:app" 8004 "_mgmt/ping"
+    run_local_service "../staging" "rs_server_staging.main:app" 8004 "_mgmt/ping"
     run_local_service "../prip" "rs_server_prip.fastapi.prip_app:app" 8005 "health"
     run_local_service "../edrs" "rs_server_edrs.fastapi.edrs_app:app" 8006 "health"
     run_local_service "../osam" "rs_server_osam.main:app" 7001 "_mgmt/ping"
