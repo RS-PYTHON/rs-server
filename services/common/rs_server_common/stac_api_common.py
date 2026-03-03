f8908c9ac2e2499da3113f42ae2b8c5ac1a6d042
Vincent Privat <vincent.privat@airbus.com>
2026-03-03 17:21:43 +0100

    pgstac replaced 0.10.0 by 0.9.10

--- services/common/rs_server_common/stac_api_common.py
+++ services/common/rs_server_common/stac_api_common.py
@@ -131,7 +131,7 @@ shapely_to_geojson_cls = {
 class Queryables(BaseModel):
     """
     BaseModel used to describe queryable holder.
-    See: site-packages/pypgstac/migrations/pgstac.0.10.0.sql
+    See: site-packages/pypgstac/migrations/pgstac.0.9.10.sql
     """
 
     id: str = Field("", alias="$id")
