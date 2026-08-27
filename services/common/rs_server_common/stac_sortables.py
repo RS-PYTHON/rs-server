46725667ed72487e2a2c7a5fe591d6218b0e7d90
astanga-cs <eastinga@AAP-CZC550735P.localdomain>
2026-08-27 15:07:55 +0300

    Fix pylint

--- services/common/rs_server_common/stac_sortables.py
+++ services/common/rs_server_common/stac_sortables.py
@@ -24,6 +24,10 @@ class RSSortablesClient(BaseSortablesClient):
         self.router_prefix = router_prefix.strip("/")
 
     def get_sortables(self, request=None, **kwargs):
+        """
+        Defines a list of sortable fields for each AUXIP/CADIP/PRIP.
+        The fields are based on sort_param_mapping available in each yaml file.
+        """
         properties = {
             "cadip": {
                 "id": {
